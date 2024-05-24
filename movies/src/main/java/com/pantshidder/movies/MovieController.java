c20a52b457fd59b7eb919364ceb1e0b5b1f10419
Inuka Perera <110172780+inuksperera@users.noreply.github.com>
2024-05-25 01:22:12 +0530

    Add files via upload

---
+++ movies/src/main/java/com/pantshidder/movies/MovieController.java
@@ -0,0 +1,28 @@
+package com.pantshidder.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "*")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{ImdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String ImdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(ImdbId), HttpStatus.OK);
+    }
+}
